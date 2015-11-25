11f6c3f7e6981d1af3c314978c34b0109e1cc768
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2015-11-25 11:08:59 +0100

    Use proper case for file names

---
+++ lib/Devel/QuickCover.pm
@@ -0,0 +1,51 @@
+package Devel::QuickCover;
+use strict;
+use warnings;
+use XSLoader;
+
+our $VERSION = '0.11';
+our $GIT_TAG = 'NONE';
+
+XSLoader::load( 'Devel::QuickCover', $VERSION );
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Devel::QuickCover - Quick & dirty code coverage for Perl
+
+=head1 VERSION
+
+Version 0.100
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 AUTHORS
+
+=over 4
+
+=item * Gonzalo Diethelm C<< gonzus AT cpan DOT org >>
+
+=back
+
+=head1 THANKS
+
+=over 4
+
+=item * Mattia Barbon
+
+=item * p5pclub
+
+=back
