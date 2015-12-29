2b9cc1af13e8c96999dcebf29779baa0b2f536c2
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2015-12-29 11:34:31 +0100

    Get ready for first CPAN release

--- lib/Devel/QuickCover.pm
+++ lib/Devel/QuickCover.pm
@@ -1,26 +1,28 @@
 package Devel::QuickCover;
+
 use strict;
 use warnings;
 use XSLoader;
 use Data::Dumper;
 
-our $VERSION = '0.11';
+our $VERSION = '0.200000';
 
 XSLoader::load( 'Devel::QuickCover', $VERSION );
 
 my %DEFAULT_CONFIG = (
-    nostart          => 0,   # Don't start gathering coverage information on import
-    nodump           => 0,   # Don't dump the coverage report at the END of the program
+    nostart          => 0,      # Don't start gathering coverage information on import
+    nodump           => 0,      # Don't dump the coverage report at the END of the program
     output_directory => "/tmp", # Write report to that directory
 );
 our %CONFIG;
 
+# A place to stash information we want dumped to the file:
 our %METADATA;
 
 sub import {
     my ($class, @opts) = @_;
 
-    die "Invalud argument to import, it takes key-value pairs. FOO => BAR" if 1==@opts % 2;
+    die "Invalid argument to import, it takes key-value pairs. FOO => BAR" if 1 == @opts % 2;
     my %options = @opts;
 
     %CONFIG = %DEFAULT_CONFIG;
@@ -31,10 +33,10 @@ sub import {
     }
 
     if (keys %options > 0) {
-        die "Invalid import option(s): ".join(',',keys %options) ;
+        die "Invalid import option(s): " . join(',', keys %options) ;
     }
 
-    if (not $CONFIG{'nostart'}) {
+    if (!$CONFIG{'nostart'}) {
         Devel::QuickCover::start();
     }
 }
@@ -46,7 +48,7 @@ sub set_metadata {
 }
 
 END {
-    if (not $CONFIG{'nodump'}) {
+    if (!$CONFIG{'nodump'}) {
         Devel::QuickCover::end();
     }
 }
@@ -65,41 +67,48 @@ Devel::QuickCover - Quick & dirty code coverage for Perl
 
 =head1 VERSION
 
-Version 0.100
+Version 0.200000
 
 =head1 SYNOPSIS
 
-The following program sets up the coverage hook on C<use> and dumps a
-report to the current working directory at the end of execution.
-
-	use Devel::QuickCover;
-        my $x = 1;
-        my $z = 1 + $x;
-
-The following program sets up the coverage hook on C<start()> and
-dumps a report to the C<output_directory> on C<end()> at which
-point the coverage hook gets uninstalled. So we only get coverage
-information for C<bar()>.
-
-       use Devel::QuickCover (nostart => 1, nodump => 1, output_directory => "some_dir/");
-       foo();
-       Devel::QuickCover::start();
-       bar();
-       Devel::QuickCover::add_metadata({ foo => "FOO", bar => "BAR" });
-       Devel::QuickCover::end();
-       baz();
-
-For now, we support calling start() only once.
+    use Devel::QuickCover;
+    my $x = 1;
+    my $z = 1 + $x;
 
 =head1 DESCRIPTION
 
-=head1 METHODS
+The following program sets up the coverage hook on C<use> and dumps
+a report (to C</tmp> by default) at the end of execution.
+
+    use Devel::QuickCover;
+    my $x = 1;
+    my $z = 1 + $x;
+
+The following program sets up the coverage hook on C<start()> and
+dumps a report to C<some_dir> on C<end()>, at which point
+the coverage hook gets uninstalled. So in this case, we only get
+coverage information for C<bar()>. We also get the specified metadata
+in the coverage information.
+
+    use Devel::QuickCover (nostart => 1, nodump => 1, output_directory => "some_dir/");
+    foo();
+    Devel::QuickCover::start();
+    bar();
+    Devel::QuickCover::add_metadata({ foo => "FOO", bar => "BAR" });
+    Devel::QuickCover::end();
+    baz();
+
+For now, we support calling start() only once.
 
 =head1 AUTHORS
 
 =over 4
 
 =item * Gonzalo Diethelm C<< gonzus AT cpan DOT org >>
+
+=item * Andreas Guðmundsson C<< andreasg AT nasarde DOT org >>
+
+=item * Andrei Vereha C<< avereha AT cpan DOT org >>
 
 =back
 
