1524ee795f79a5f3be02fe264e421a1d7807459a
Gonzalo Diethelm <gonzalo.diethelm@booking.com>
2015-12-30 17:11:29 +0100

    Move includes only where they are needed

--- glog.c
+++ glog.c
@@ -1,16 +1,19 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "glog.h"
 
 #ifndef GLOG_SHOW
 
-void glog(const char* fmt, ...) {
+void glog(const char* fmt, ...)
+{
 }
 
 #else
 
-void glog(const char* fmt, ...) {
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void glog(const char* fmt, ...)
+{
   va_list args;
   va_start(args, fmt);
   fprintf(stderr, "{%lu} ", (unsigned long) getpid());
